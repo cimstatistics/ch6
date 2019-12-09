c043853d139be9705e01076be855a74183919e81
Behrang <behrang.mahjani@gmail.com>
2019-12-09 13:37:03 -0500

    Update 6.3.36.R

--- 6.3.36.R
+++ 6.3.36.R
@@ -9,3 +9,11 @@ lines(1:16+0.4,W$s2,"h")
 ### wavelet thresholding for the transforms
 W.sure <- sure.thresh(W,max.level=2,hard=TRUE)
 plot(1:32,W.sure$d1, ylim=c(-0.5,5),"h")
+points(1:16+0.2,W.sure$d2,"h")
+lines(1:16+0.4,W.sure$s2,"h")
+### comparison smoothing with sure and with out
+plot(time[60:123],mm,"p",xlab="",ylab="")
+WA <- mra(mm,J=3)
+lines(time[60:123],WA$S3)
+WA.sure <- sure.thresh(WA,max.level=2,hard=TRUE)
+lines(time[60:123],W1A.sure$S3)
